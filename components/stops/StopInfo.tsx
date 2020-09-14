17bb09a77d9d0769dc81dde26a9e82ede168fb91
Zac Scott <scottzach1@myvuw.ac.nz>
2020-09-15 03:57:09 +1200

    Adds basic Stop Info information about a stop.
    In this commit, we populate the StopInfo component
    with basic information about a stop. We do not add
    any style sheets or complexity to the component.

--- components/stops/StopInfo.tsx
+++ components/stops/StopInfo.tsx
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import {Card} from "react-native-elements";
 import {StackNavigationProp} from "@react-navigation/stack";
+import {Text, View} from "../Themed";
+import {ActivityIndicator} from "react-native";
 
 interface Props {
     navigation: StackNavigationProp<any>,
@@ -19,10 +21,35 @@ class StopInfo extends Component<Props, State> {
         this.state = {}
     }
 
+    getStopName() {
+        if (!this.props.stopData?.Stop?.Name) return 'Unknown';
+        else return this.props.stopData.Stop.Name;
+    }
+
+    getFareZone() {
+        if (!this.props.stopData?.Stop?.Farezone) return 'Unknown';
+        else return this.props.stopData.Stop.Farezone;
+    }
+
+    getNumberNotices() {
+        if (!this.props.stopData?.Notices) return 'Unknown';
+        else return this.props.stopData.Notices.length;
+    }
+
     render() {
+        console.log('info', this.props.stopData);
+
         return (
             <Card>
-                <Card.Title>Stop {this.props.code}</Card.Title>
+                <Card.Title>{this.getStopName()}</Card.Title>
+                <Card.Divider/>
+                {this.props.stopData ?
+                    <View>
+                        <Text>Code {this.props.code}</Text>
+                        <Text>Faze zone {this.getFareZone()}</Text>
+                        <Text>Notices {this.getNumberNotices()}</Text>
+                    </View>
+                    : <ActivityIndicator/>}
             </Card>
         );
     }
