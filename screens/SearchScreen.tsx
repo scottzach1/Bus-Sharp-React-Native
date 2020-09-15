2d78bce40dc8bab9f8d8e1491ca65c8e47d08cf5
Harrison <cookharr@ecs.vuw.ac.nz>
2020-09-16 03:54:16 +1200

    Adds Service and Stop List Containers to the SearchScreen

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -2,11 +2,13 @@ import React, {Component} from "react";
 import {ScrollView, StyleSheet} from "react-native";
 import {StackNavigationProp} from "@react-navigation/stack";
 import {getAllServices, getAllStops} from "../external/StorageManager";
-import {SearchBar} from "react-native-elements";
+import {Card, SearchBar} from "react-native-elements";
 import {View} from "../components/common/Themed";
 import SearchTabSearchbarDescriptionCard from "../components/search-tab/SearchTabSearchbarDescription";
 import SearchTabTabsDescription from "../components/search-tab/SearchTabTabsDescription";
 import SearchTabFilter from "../components/search-tab/SearchTabFilter";
+import ServiceListContainer, {ServiceListProp} from "../components/services/ServiceListContainer";
+import StopListContainer, {StopListProp} from "../components/stops/StopListContainer";
 
 interface Props {
     navigation: StackNavigationProp<any>,
@@ -18,8 +20,15 @@ interface State {
     stopsErrorMessage: string | null,
     servicesErrorMessage: string | null,
     searchText: string,
-    selectedIndex: number
+    selectedIndex: number,
+    stopListElements: StopListProp[],
+    serviceListElements: ServiceListProp[]
 }
+
+let prevSearch: string = ""
+let prevFilter: number = 0
+let remainingStops: number = 0
+let remainingServices: number = 0
 
 class SearchScreen extends Component<Props, State> {
 
@@ -32,7 +41,9 @@ class SearchScreen extends Component<Props, State> {
             stopsErrorMessage: null,
             servicesErrorMessage: null,
             searchText: "",
-            selectedIndex: 0
+            selectedIndex: 0,
+            stopListElements: [],
+            serviceListElements: []
         }
     }
 
@@ -60,7 +71,83 @@ class SearchScreen extends Component<Props, State> {
         this.setState({selectedIndex: newValue})
     }
 
+    /**
+     * Creates new ServiceListProps and StopListProps to display as potential results to the user.
+     */
+    async updateSearchResults() {
+        if (!this.state.stopsData || !this.state.servicesData ||
+            (prevSearch === this.state.searchText && prevFilter === this.state.selectedIndex)) {
+            return
+        }
+
+        prevSearch = this.state.searchText
+        prevFilter = this.state.selectedIndex
+
+        this.updateStopResults()
+        this.updateServiceResults()
+    }
+
+    /**
+     * Maps all stops to searchable items, then these searchable items are then filtered based on the users
+     * input. These results are then mapped to StopListProps
+     */
+    updateStopResults() {
+        // Get a set of filtered STOP search items
+        let filterStops: SearchItem[] = Object.entries(this.state.stopsData)
+            .map((i: any) => {
+                let item = i[1]
+                return new SearchItem(item.stop_id + "", item.stop_name + "", true)
+            })
+            .filter((searchItem: SearchItem) => this.filterItem(searchItem))
+
+        let sliceStops: StopListProp[] = filterStops
+            .slice(0, Math.min(filterStops.length, 51))
+            .map((item: SearchItem) => new StopListProp(item.name, item.code))
+
+        this.setState({stopListElements: sliceStops})
+    }
+
+    /**
+     * Maps all services to searchable items, then these searchable items are then filtered based on the users
+     * input. These results are then mapped to ServiceListProps
+     */
+    updateServiceResults() {
+        // Get a set of filtered SERVICE search items
+        let filterServices: SearchItem[] = Object.entries(this.state.servicesData)
+            .map((i: any) => {
+                let item = i[1]
+                return new SearchItem(item.route_id + "", item.route_long_name + "", false)
+            })
+            .filter((searchItem: SearchItem) => this.filterItem(searchItem))
+
+        let sliceServices: ServiceListProp[] = filterServices
+            .slice(0, Math.min(filterServices.length, 51))
+            .map((item: SearchItem) => new ServiceListProp(item.name, item.code))
+
+        // Update the list elements
+        this.setState({serviceListElements: sliceServices})
+    }
+
+    filterItem(item: SearchItem) {
+        const filter: number = this.state.selectedIndex
+        const searchText: string = this.state.searchText.toLowerCase();
+
+        if ((filter === 1 && !item.isStop) || (filter === 2 && item.isStop)) {
+            return false;
+        }
+
+        if (searchText.length) {
+            for (const key of item.searchText) {
+                if ((filter !== 4) ? key.includes(searchText) : key.startsWith(searchText))
+                    return true;
+            }
+        }
+        return false;
+    }
+
     render() {
+        this.updateSearchResults().then()
+
         return (
             <View style={styles.container}>
                 <SearchBar
@@ -76,9 +163,42 @@ class SearchScreen extends Component<Props, State> {
                     {!this.state.searchText && (
                         <SearchTabTabsDescription/>
                     )}
+                    {this.state.serviceListElements.length && (
+                        <Card>
+                            <Card.Title>Services</Card.Title>
+                            <Card.Divider/>
+                            <ServiceListContainer
+                                navigation={this.props.navigation}
+                                services={this.state.serviceListElements}/>
+                        </Card>
+                    )}
+                    {this.state.stopListElements.length && (
+                        <Card>
+                            <Card.Title>Stops</Card.Title>
+                            <Card.Divider/>
+                            <StopListContainer
+                                navigation={this.props.navigation}
+                                stops={this.state.stopListElements}/>
+                        </Card>
+                    )}
                 </ScrollView>
             </View>
         );
+    }
+}
+
+
+class SearchItem {
+    searchText: string[];
+    code: string;
+    name: string;
+    isStop: boolean;
+
+    constructor(code: string, name: string, isStop: boolean) {
+        this.searchText = [code.toLowerCase(), name.toLowerCase()];
+        this.isStop = isStop;
+        this.name = name;
+        this.code = code;
     }
 }
 
