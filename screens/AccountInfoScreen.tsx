5cfc623afcb9796bfcb97f4a7f356034790b37d5
Zac Scott <scottzach1@myvuw.ac.nz>
2020-09-16 03:56:08 +1200

    Adds Account Profile Screen
    In this commit we render information regarding the user account in the
    account info screen. The layout is very similar to the previous project
    where we show a table of all the user data within the cloud.

--- screens/AccountInfoScreen.tsx
+++ screens/AccountInfoScreen.tsx
@@ -1,28 +1,77 @@
 import React, {Component} from "react";
-import {StyleSheet, Text, View} from "react-native";
+import {View} from "react-native";
+import {Card} from "react-native-elements";
+import {UserContext} from "../providers/UserProvider";
+import {getUserDocument} from "../external/Firebase";
+import {Text} from "../components/common/Themed";
 
 interface Props {
 }
 
 interface State {
+    doc: any | null,
 }
 
 class AccountInfoScreen extends Component<Props, State> {
+    static contextType = UserContext;
+
+    constructor(props: Readonly<Props>) {
+        super(props);
+
+        this.state = {
+            doc: null,
+        }
+    }
+
+    componentDidMount() {
+        this.getUserDocument();
+    }
+
+    getUserDocument() {
+        if (this.context && !this.state.doc) {
+            getUserDocument(this.context).then((doc: any | null) => {
+                this.setState({doc: doc});
+            });
+        }
+    }
+
+    getName() {
+        return (this.state.doc) ? this.state.doc.displayName : undefined;
+    }
+
+    generateTable() {
+        const doc = this.state.doc;
+        if (!doc) return undefined;
+
+        let listItems: any[] = [];
+
+        for (let property in doc) {
+            if (!doc.hasOwnProperty(property)) continue;
+            listItems.push(
+                <Text>
+                    <b>{property}:</b> {doc[property]}
+                </Text>
+            )
+        }
+
+        return listItems;
+    }
+
     render() {
+        this.getUserDocument();
+
+        console.log('doc', this.state.doc);
+
         return (
-            <View style={styles.container}>
-                <Text>Account Info!</Text>
+            <View>
+                <Card>
+                    <Card.Title>User Profile {this.getName()}</Card.Title>
+                    <Card.Divider/>
+                    {this.generateTable()}
+                </Card>
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
 
 export default AccountInfoScreen;
